67378225210e48c1299e95cc9e90a6f8461a19a8
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-04-01 22:11:13 -0600

    Add hash about dinosaur description

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur_description = {
+                        "Official Name" => "Tyrannosaurus Rex",
+                        "Nick Name" => "T.rex",
+                        "Height in feet" => 20,
+                        "Weight in tons" => 9,
+                        "is instinct" => true,
+                        "is big" => true,
+                        "Favorite Meal" => ["Edmontosaurus", "Triceratops"],
+                        "Fossils Found" => ["Montana", "Texas", "Utah"]
+}
