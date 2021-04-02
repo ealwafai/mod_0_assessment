db775dec8a8504aa2b7a17783fbc57d5125ca90d
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-04-01 21:54:24 -0600

    Add variables and an array

---
+++ variables.rb
@@ -0,0 +1,12 @@
+movie = "The Dark Knight"
+is_entertaining = true
+movie = "Dumb and Dumber"
+
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+
+favorite_foods = ["Hamburger", "Pizza", "Falafel"]
+p favorite_foods.shift
+
+p movie.length
