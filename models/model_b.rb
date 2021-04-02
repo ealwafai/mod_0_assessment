8ca35fcec08d695791d5fd4ea007d679ca03e047
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-04-01 22:24:13 -0600

    Add developer characteristics

---
+++ models/model_b.rb
@@ -0,0 +1,11 @@
+developer_characteristics = {
+                            "Full Name" => "Ezze Alwafai",
+                            "School Name" => "Turing",
+                            "GPA Minimum" =>3.75,
+                            "Weekly Hours Worked" =>50,
+                            "Honest" => true,
+                            "Unapproachable" => false,
+                            "Personal Charateristics" => ["passionate", "open-minded", "data-driven"],
+                            "Software Product" => ["elegant", "creative", "anticipate needs"]
+}
+p developer_characteristics
